7a5e1589cd87b49c38fb07eb74f35c091b171a40
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-14 18:00:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Write your code in this file!
+
+  
+  //if gives customers a free sample if the ride is less than or equal to 400 feet
+  //charges 30 dollars for a distance over 2000 feet
+  //does not allow rides over 2500 feet
+ 
+function scuberGreetingForFeet(ride){
+    let greeting="";
+   if  (ride <=  400) {
+      greeting= 'This one is on me!';
+      }
+      
+     if (ride > 2000){
+        greeting='I will gladly take your thirty bucks.'  
+  }
+      
+      if (ride >2500) {
+        greeting="No can do."
+      }
+  return greeting
+    
+}
+  
+  
+function  ternaryCheckCity(city) {
+  
+  let response= city==="NYC" ? "Ok, sounds good." : "No go.";
+  return response
+}
+
+function switchOnCharmFromTip(tip){
+  
+  
+  
+  let response= ""
+  
+    switch (tip) {
+    case ('generous'):
+      // Statements
+     response= "Thank you so much."
+    break;
+    case ('not as generous'):
+      // Statements
+       response= "Thank you."
+    break;
+    default:
+       response="Bye."
+    break;
+}
+ return response
+  
+}
