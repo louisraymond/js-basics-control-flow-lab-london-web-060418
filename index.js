5dc1f740c9010760c76de6fe8a9976d07d8692ba
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-14 17:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Write your code in this file!
 def scuberGreetingForFeet(ride){
   
   //if gives customers a free sample if the ride is less than or equal to 400 feet
   //charges 30 dollars for a distance over 2000 feet
   //does not allow rides over 2500 feet
  
-def    
-  if  ride <=  400 {
+function scuberGreetingForFeet(ride){
+  if  ride <=  400; {
     'This one is on me!'
   }
   
